eedf18ff33c8d51701aa398fd90b630427440d1f
fab6887 <fab6887@gmail.com>
2017-05-16 13:55:13 +0200

    Update 20161227200322_populate_description_on_sensors.rb
    Fix purpose.
    Need to find the entries by its ID because sensors name is no more like "ESPX03...." but "Outside X". The first entry are reserved for the forecast.

--- db/migrate/20161227200322_populate_description_on_sensors.rb
+++ db/migrate/20161227200322_populate_description_on_sensors.rb
@@ -1,7 +1,7 @@
 class PopulateDescriptionOnSensors < ActiveRecord::Migration[5.0]
   def up
-    Sensor.find_by_name("ESP_0BA4E4").update_column(:description, 'Kitchen, on top of plant light')
-    Sensor.find_by_name("ESP_E2106F").update_column(:description, 'Living Room, on east wall')
+    Sensor.find_by_id(2).update_column(:description, 'Kitchen, on top of plant light')
+    Sensor.find_by_id(3).update_column(:description, 'Living Room, on east wall')
   end
 
   def down
